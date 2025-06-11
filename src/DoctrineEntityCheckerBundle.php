554c77120bf26b7f176e456b2d56df4062625ea1
github-ci <you@example.com>
2025-06-11 17:53:19 +0800

    upgrade

--- src/DoctrineEntityCheckerBundle.php
+++ src/DoctrineEntityCheckerBundle.php
@@ -2,8 +2,16 @@
 
 namespace Tourze\DoctrineEntityCheckerBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class DoctrineEntityCheckerBundle extends Bundle
+class DoctrineEntityCheckerBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            DoctrineBundle::class => ['all' => true],
+        ];
+    }
 }
